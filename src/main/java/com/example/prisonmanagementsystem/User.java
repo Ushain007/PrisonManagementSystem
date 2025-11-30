6008d8d3a2d53d48e60d7e6a1c34dbc29a6a48c8
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 00:39:56 +0600

    send

--- src/main/java/com/example/prisonmanagementsystem/User.java
+++ src/main/java/com/example/prisonmanagementsystem/User.java
@@ -1,4 +1,87 @@
 package com.example.prisonmanagementsystem;
 
 public class User {
+    private String id, Name, phoneNo, email, address, gender, password;
+    private LocalDate dateOfBirth;
+
+    public User(String id, String name, String email, String phoneNo, String address, String gender, String password, LocalDate dateOfBirth) {
+        this.id = id;
+        this.Name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.address = address;
+        this.gender = gender;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", Name='" + Name + '\'' +
+                ", phoneNo='" + phoneNo + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", gender='" + gender + '\'' +
+                ", password='" + password + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                '}';
+    }
 }
