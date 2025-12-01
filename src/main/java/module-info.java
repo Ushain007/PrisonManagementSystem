8eb7dca3ae2861c5734854ae2c3e8521b3c771d5
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 15:00:52 +0600

    k

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.prisonmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.prisonmanagementsystem to javafx.fxml;
     opens com.example.prisonmanagementsystem.Librarian to javafx.fxml, javafx.base;
     opens com.example.prisonmanagementsystem.Warden to javafx.fxml, javafx.base;
