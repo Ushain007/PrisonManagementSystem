4ea97169fb7181a686393a9986de5410ac8ef9bd
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 13:49:09 +0600

    g

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/CreateNewUserController.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/CreateNewUserController.java
@@ -3,5 +3,20 @@ package com.example.prisonmanagementsystem.Ushain_2320676.Warden;
 public class CreateNewUserController
 {
     @javafx.fxml.FXML
+    private Label outputLabel;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField fullNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TextField contactTextField;
+    @javafx.fxml.FXML
+    private Label generatedUserID;
+    @javafx.fxml.FXML
+    private DatePicker dobDatePicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }}
