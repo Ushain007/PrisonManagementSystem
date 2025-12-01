47c25316820d432a46d1c9c5e6d6b61a9775464d
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 15:26:05 +0600

    k

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/LibrarianDashboardController.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/LibrarianDashboardController.java
@@ -4,4 +4,37 @@ public class LibrarianDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void bookManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void userManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void categoryManagementOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void notificationsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void librarySummaryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewBookDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void bookReturnsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueBooksOnAction(ActionEvent actionEvent) {
+    }
+}
