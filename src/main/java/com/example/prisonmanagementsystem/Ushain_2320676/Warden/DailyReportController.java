4ae890dc330a25462beb5e3fa9b96005248877d5
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 14:06:11 +0600

    000

---
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/DailyReportController.java
@@ -0,0 +1,32 @@
+package com.example.prisonmanagementsystem.Ushain_2320676.Warden;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+
+public class DailyReportController
+{
+    @javafx.fxml.FXML
+    private DatePicker reportDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox reportTypeComboBox;
+    @javafx.fxml.FXML
+    private TextArea summaryTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void generateReportOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadAsFIleOnAction(ActionEvent actionEvent) {
+    }
+}
