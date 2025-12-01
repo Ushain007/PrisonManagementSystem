0d498e97fb83a49510f463491ff78deb4ad893ec
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 14:05:51 +0600

    chnaged

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/WardenDashboardController.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/WardenDashboardController.java
@@ -4,4 +4,45 @@ public class WardenDashboardController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void announcementsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void visitorRequestsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogout(ActionEvent actionEvent) {
+    }
+
+    @Deprecated
+    public void releaseOrParoleRequestsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void disciplineRecordsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createNewUserOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignShiftOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dailyReportsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void assignOrTransferPrisonerOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void maintenanceManagementOnAction(ActionEvent actionEvent) {
+    }
+}
