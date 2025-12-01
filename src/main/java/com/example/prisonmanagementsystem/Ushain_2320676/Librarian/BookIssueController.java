15add6df7cec748c128a16e4a32fff05b45798d9
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 15:26:25 +0600

    G2U2

---
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/BookIssueController.java
@@ -0,0 +1,48 @@
+package com.example.prisonmanagementsystem.Ushain_2320676.Librarian;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BookIssueController
+{
+    @javafx.fxml.FXML
+    private Label bookAuthorLabel;
+    @javafx.fxml.FXML
+    private TextField borrowerSearchTextField;
+    @javafx.fxml.FXML
+    private DatePicker returnDatePicker;
+    @javafx.fxml.FXML
+    private TextField bookSearchTextField;
+    @javafx.fxml.FXML
+    private Label bookStatusLabel;
+    @javafx.fxml.FXML
+    private Label bookTitleLabel;
+    @javafx.fxml.FXML
+    private DatePicker issueDatePicker;
+    @javafx.fxml.FXML
+    private Label borrowerTypeLabel;
+    @javafx.fxml.FXML
+    private Label borrowerNameLabel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyUserOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchBookOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmIssueOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/BookManagementController.java
@@ -0,0 +1,36 @@
+package com.example.prisonmanagementsystem.Ushain_2320676.Librarian;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BookManagementController
+{
+    @javafx.fxml.FXML
+    private TextField authorNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox categoryComboBox;
+    @javafx.fxml.FXML
+    private Label bookAddedMessageOutput;
+    @javafx.fxml.FXML
+    private TextField publicationYearTextField;
+    @javafx.fxml.FXML
+    private TextField bookTitleTextField;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+    @javafx.fxml.FXML
+    private TextField bookSectionTextField;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addNewBookOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backOnClick(ActionEvent actionEvent) {
+    }
+}
