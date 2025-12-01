36012dcf99aaeaa1864f39933ba83eed507b98f7
Ushain007 <ushain.mi293@hotmail.com>
2025-12-01 13:48:26 +0600

    g

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/CellTransferController.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Warden/CellTransferController.java
@@ -3,6 +3,23 @@ package com.example.prisonmanagementsystem.Ushain_2320676.Warden;
 public class CellTransferController
 {
     @javafx.fxml.FXML
+    private TextField searchByIDNameTextfield;
+    @javafx.fxml.FXML
+    private Label cellOutputLabel;
+    @javafx.fxml.FXML
+    private Label securityLevelOutputLabel;
+    @javafx.fxml.FXML
+    private TextField cellTransferTextField;
+    @javafx.fxml.FXML
+    private Label transferSuccessfulOutput;
+    @javafx.fxml.FXML
+    private Label blockOutputLabel;
+    @javafx.fxml.FXML
+    private Label nameOutputLabel;
+    @javafx.fxml.FXML
+    private TextField blockTransferTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
     }
 
