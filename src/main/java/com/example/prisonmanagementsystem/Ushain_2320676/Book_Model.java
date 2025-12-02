f8d7b8bd0334a5492706c5616d195ab053d8e887
Ushain007 <ushain.mi293@hotmail.com>
2025-12-03 00:01:09 +0600

    nice

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Book_Model.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Book_Model.java
@@ -1,4 +1,20 @@
 package com.example.prisonmanagementsystem.Ushain_2320676;
 
-public class Book_Model {
-}
+import java.io.Serializable;
+
+public class Book_Model implements Serializable {
+    private String title;
+    private String author;
+    private String category;
+    private int publicationYear;
+    private int quantity;
+    private String shelfSection;
+
+    public Book_Model(String title, String author, String category, int publicationYear, int quantity, String shelfSection) {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.publicationYear = publicationYear;
+        this.quantity = quantity;
+        this.shelfSection = shelfSection;
+    }
