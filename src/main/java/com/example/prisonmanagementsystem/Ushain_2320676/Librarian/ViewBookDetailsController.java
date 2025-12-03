d0ad9fb882ffb8694ee6e54ce23e4a2d74092609
Ushain007 <ushain.mi293@hotmail.com>
2025-12-03 14:21:31 +0600

    go

--- src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/ViewBookDetailsController.java
+++ src/main/java/com/example/prisonmanagementsystem/Ushain_2320676/Librarian/ViewBookDetailsController.java
@@ -4,4 +4,18 @@ public class ViewBookDetailsController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LibrarianDashboardFXML.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void searchBookOnAction(ActionEvent actionEvent) {
+    }
+}
